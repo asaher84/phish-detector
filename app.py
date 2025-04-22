0800726c69e286818cb08966a1881cc5c49c184d
Aditya Santosh Aher <129608959+asaher84@users.noreply.github.com>
2025-04-22 13:23:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,12 +31,12 @@ def predict():
         y_pred =gbc.predict(x)[0]
             #1 is safe
             #-1 is unsafe
-        #y_pro_phishing = gbc.predict_proba(x)[0,0]
-        #y_pro_non_phishing = gbc.predict_proba(x)[0,1]
-            # if(y_pred ==1 ):
-        #3pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
-        #xx =y_pred
-        name=convertion(url,int(y_pred))
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        if(y_pred ==1 ):
+            3pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
+            xx =y_pred
+            name=convertion(url,int(y_pred))
         return render_template("index.html", name=name)
 @app.route('/usecases', methods=['GET', 'POST'])
 def usecases():
